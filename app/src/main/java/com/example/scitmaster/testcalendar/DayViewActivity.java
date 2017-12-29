d97ef84d7961d2db9a4b471396c24477f2a5dfee
TreeLatte <jjh8623@gmail.com>
2017-12-29 17:02:53 +0900

    temp test

---
+++ app/src/main/java/com/example/scitmaster/testcalendar/DayViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.scitmaster.testcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dayview);
+    }
+
+}
--- app/src/main/java/com/example/scitmaster/testcalendar/MainActivity.java
+++ app/src/main/java/com/example/scitmaster/testcalendar/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.scitmaster.testcalendar;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = (MaterialCalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Intent intent = new Intent(MainActivity.this,DayViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
